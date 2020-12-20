d5eedab18076bb96e49ed9af738f87ec6ee83f20
AlexandraBasakina <56187112+AlexandraBasakina@users.noreply.github.com>
2020-12-20 23:56:42 +0300

    Add files via upload

--- complingv.py
+++ complingv.py
@@ -1,11 +1,16 @@
-file1 = open('TolstoyLevVoynaIMirTom2.txt', 'r')
-text = file1.readlines()
-for word in text:
-    print(word)
-
-file2 = open('auxiliarypartofspeech.txt', 'r')
-text1 = file2.readlines()
-for word1 in text1:
-    word2 = str(word1).encode('cp1251').decode('utf8')
-    print(word2)
-
+import codecs
+import re
+if __name__ == "__main__":
+    file1 = codecs.open('TolstoyLevVoynaIMirTom2.txt', 'r', encoding='cp1251', errors='ignore')
+    text = file1.read()
+    file2 = codecs.open('auxiliarypartofspeech.txt', 'r', encoding='utf8', errors='ignore')
+    text1 = [word.strip() for word in file2.readlines()]
+    text1 = text1
+    for word in text1:
+        text = re.sub(rf"\b{word.lower()}\b", "", text)
+        text = re.sub(rf"\b{word.upper()}\b", "", text)
+ 
+    print(text)
+    file3 = open('ClearTolstoyLevVoynaIMirTom2.txt', 'w')
+    file3.write(text)
+    file3.close()
