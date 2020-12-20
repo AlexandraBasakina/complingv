3eee4e6d8391d51dd3154e324911da77fd8731f9
AlexandraBasakina <56187112+AlexandraBasakina@users.noreply.github.com>
2020-12-20 21:31:28 +0300

    Add files via upload

--- complingv.py
+++ complingv.py
@@ -1,13 +1,11 @@
-
 file1 = open('TolstoyLevVoynaIMirTom2.txt', 'r')
 text = file1.readlines()
 for word in text:
     print(word)
+
 file2 = open('auxiliarypartofspeech.txt', 'r')
 text1 = file2.readlines()
 for word1 in text1:
     word2 = str(word1).encode('cp1251').decode('utf8')
     print(word2)
-file3 = open('rom.txt', 'w')
 
-file3.close()
